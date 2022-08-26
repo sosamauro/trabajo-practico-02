aec289f9d86aaf38b57c46d1bd8a7b45a3594ee2
Mauro Sosa <mauro4sosa@gmail.com>
2022-08-26 18:43:54 -0300

    Add project files.

---
+++ Ejercicio01/Circulo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    internal class Circulo
+    {
+        private double iRadio;
+        private Punto iCentro;
+        private double iArea;
+        private double iPerimetro;
+
+        public Circulo(Punto pCentro, double pRadio)
+        {
+            this.iRadio = pRadio;
+            this.iCentro = pCentro;
+        }
+
+        public Circulo(double pX, double pY, double pRadio)
+        {
+            this.iCentro.X = pX;
+            this.iCentro.Y = pY;
+            this.iRadio = pRadio;
+        }
+
+        public double Radio
+        {
+            get { return this.iRadio; }
+            set { this.iRadio = value; }
+        }
+
+        public Punto Centro
+        {
+            get { return this.iCentro; }
+            set { this.iCentro = value; }
+        }
+
+        public double Area
+        {
+            get { return this.iArea; }
+            set { this.iArea = value; }
+        }
+
+        public double Perimetro
+        {
+            get { return this.iPerimetro; }
+            set { this.iPerimetro = value; }
+        }
+
+
+    }
+}
---
+++ Ejercicio01/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Ejercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double X1,Y1,X2,Y2;
+
+            Console.Clear();
+            Console.WriteLine("Seleccione una opción")
+            Console.WriteLine("")
+        
+            Console.WriteLine("Ingrese el primer punto.");
+            Console.Write("Coordenada en X: ");
+            X1 = Double.Parse(Console.ReadLine());
+            Console.Write("Coordenada en Y: ");
+            Y1 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Ingrese el segundo punto.");
+            Console.Write("Coordenada en X: ");
+            X2 = Double.Parse(Console.ReadLine());
+            Console.Write("Coordenada en Y: ");
+            Y2 = Double.Parse(Console.ReadLine());
+
+            var punto1 = new Punto(X1, Y1);
+            var punto2 = new Punto(X2, Y2);
+            var distancia = punto2.CalcularDistanciaDesde(punto1);
+
+            Console.WriteLine();
+            Console.WriteLine("La distancia entre los dos puntos es: " + distancia);
+        }
+    }
+}
