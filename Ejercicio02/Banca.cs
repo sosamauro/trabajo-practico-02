9984f74863ed816744e95a32ad77454578beb4d5
Mauro Sosa <mauro4sosa@gmail.com>
2022-08-31 18:48:59 -0300

    Ejercicio02 creado

---
+++ Ejercicio02/Banca.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio02
+{
+    internal class Banca
+    {
+        private string iNumero, iTitular;
+
+        public Banca(string pNumero, string pTitular)
+        {
+            this.iNumero = pNumero;
+            this.iTitular = pTitular;
+        }
+
+        public string Numero
+        {
+            get { return this.iNumero; }
+            set { this.iNumero = value; }
+        }
+
+        public string Titular
+        {
+            get { return this.iTitular; }
+            set { this.iTitular = value; }
+        }
+    }
+}
