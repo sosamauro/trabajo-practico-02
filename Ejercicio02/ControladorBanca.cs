5d53fd3e756cb752264bf0ac5602c2aa9b6ae316
Mauro Sosa <mauro4sosa@gmail.com>
2022-09-03 14:11:44 -0300

    Subo archivos

---
+++ Ejercicio02/ControladorBanca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio02
+{
+    public static class ControladorBanca
+    {
+        public static void CrearBanca(string pNumero, string pTitular)
+        {
+            Banca banca = new Banca(pNumero, pTitular);
+        }
+    }
+}
---
+++ Ejercicio02/Cuenta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio02
+{
+    public class Cuenta
+    {
+        private double iSaldo;
+        private Moneda iMoneda;
+
+        public Cuenta(Moneda pMoneda)
+        {
+            this.iMoneda = pMoneda;
+            this.iSaldo = 0.0;
+        }
+
+        public Cuenta(double pSaldoInicial, Moneda pMoneda)
+        {
+            this.iSaldo = pSaldoInicial;
+            this.iMoneda = pMoneda;
+        }
+
+        public double Saldo
+        {
+            get { return this.iSaldo; }
+            set { this.iSaldo = value; }
+        }
+
+        public void AcreditarSueldo(double pSaldo)   
+        {
+            
+        }
+
+        public Boolean DebitarSueldo(double pSaldo)
+        {
+            return true;
+        }
+    }
+}
