072f35ecc09517dcfccb0f62dcd83dab30167d4d
Mauro Sosa <mauro4sosa@gmail.com>
2022-09-05 10:53:45 -0300

    Agregado lo de Repository

--- Ejercicio01/PantallaCirculo.cs
+++ Ejercicio01/PantallaCirculo.cs
@@ -1,32 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ejercicio01
+﻿namespace Ejercicio01
 {
-    internal static class PantallaCirculo
+  internal static class PantallaCirculo
+  {
+    public static void MostrarPantalla()
     {
-        public static void MostrarPantalla()
-        {
-            Console.Clear();
-            Console.WriteLine("Ingrese el centro del círculo");
-            Console.Write("Coordenada en X: ");
-            double x = Double.Parse(Console.ReadLine());
-            Console.Write("Coordenada en Y: ");
-            double y = Double.Parse(Console.ReadLine());
-            Console.WriteLine();
-            Console.Write("Ingrese el radio del círculo: ");
-            double r = Double.Parse(Console.ReadLine());
+      Console.Clear();
+      Console.WriteLine("Ingrese el centro del círculo");
+      Console.Write("Coordenada en X: ");
+      double x = Double.Parse(Console.ReadLine());
+      Console.Write("Coordenada en Y: ");
+      double y = Double.Parse(Console.ReadLine());
+      Console.WriteLine();
+      Console.Write("Ingrese el radio del círculo: ");
+      double r = Double.Parse(Console.ReadLine());
 
-            Console.WriteLine();
-            Console.WriteLine("El área del círculo es: " + ControladorCirculo.CalcularArea(x, y, r));
-            Console.WriteLine("El perímetro del círculo es: " + ControladorCirculo.CalcularPerimetro(x, y, r));
-            Console.WriteLine();
-            Console.Write("Presione ENTER para continuar...");
-            Console.ReadKey();
-            Console.Clear();
-        }
+      Console.WriteLine();
+      Console.WriteLine("El área del círculo es: " + ControladorCirculo.CalcularArea(x, y, r));
+      Console.WriteLine("El perímetro del círculo es: " + ControladorCirculo.CalcularPerimetro(x, y, r));
+      Console.WriteLine();
+      Console.Write("Presione ENTER para continuar...");
+      Console.ReadKey();
+      Console.Clear();
     }
+  }
 }
--- Ejercicio02/ControladorBanca.cs
+++ Ejercicio02/ControladorBanca.cs
@@ -8,9 +8,29 @@ namespace Ejercicio02
 {
     public static class ControladorBanca
     {
+        private static readonly List<Banca> _bancaList = new List<Banca>();
         public static void CrearBanca(string pNumero, string pTitular)
         {
             Banca banca = new Banca(pNumero, pTitular);
+            AgregarBanca(banca);
+        }
+
+        public static void AgregarBanca(Banca banca)
+        {
+            _bancaList.Add(banca);
+        }
+
+        public static Banca ObtenerBanca(string pNumero)
+        {
+            foreach (Banca banca in _bancaList)
+            {
+                if (banca.Numero == pNumero)
+                {
+                    return banca;
+                }
+            }
+
+            return null;
         }
     }
 }
