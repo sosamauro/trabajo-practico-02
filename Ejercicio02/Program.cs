423ad7e1343a87e78c2101d603d473b7f87cbe47
Mauro Sosa <mauro4sosa@gmail.com>
2022-09-05 16:59:12 -0300

    codigo estructurado

--- Ejercicio02/Program.cs
+++ Ejercicio02/Program.cs
@@ -1,47 +1,15 @@
-﻿namespace Ejercicio02
+﻿using Ejercicio02.Repositorio;
+using Ejercicio02.Pantallas;
+using Ejercicio02.Controladores;
+using Ejercicio02.Clases;
+
+namespace Ejercicio02
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string opcion;
-            Boolean salir = false;
-            while (salir == false)
-            {
-                Console.Clear();
-                Console.WriteLine("Seleccione una opción:");
-                Console.WriteLine();
-                Console.WriteLine("1) Crear una cuenta");
-                Console.WriteLine("2) Realizar una operación");
-                Console.WriteLine();
-                Console.WriteLine("3) Salir");
-                Console.WriteLine();
-                Console.Write("Opción: ");
-                opcion = Console.ReadLine();
-
-                switch (opcion)
-                {
-                    case "1":
-                        PantallaCrearCuenta.CrearCuenta();
-                        break;
-                    case "2":
-                        PantallaOperaciones.OperarConCuenta();
-                        break;
-                    case "3":
-                        salir = true;
-                        Console.Clear();
-                        break;
-                    default:
-                        Console.Clear();
-                        Console.WriteLine("Ingrese una opción válida.");
-                        Console.WriteLine();
-                        Console.Write("Presione ENTER para continuar...");
-                        Console.ReadKey();
-                        Console.Clear();
-                        break;
-                }
-
-            }
+            PantallaPrincipal.MostrarPantalla();
         }
     }
 }
