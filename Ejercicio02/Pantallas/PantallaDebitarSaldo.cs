d396f8602fee6b343219a534c9a61e496fbf2a2d
Mauro Sosa <mauro4sosa@gmail.com>
2022-09-07 09:49:46 -0300

    Pantalla debitar saldo y transferencia

---
+++ Ejercicio02/Pantallas/PantallaDebitarSaldo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio02.Clases;
+using Ejercicio02.Metodos;
+
+namespace Ejercicio02.Pantallas
+{
+    public static class PantallaDebitarSaldo
+    {
+        public static void MostrarPantalla(Cuenta pCuenta, string pMoneda)
+        {
+            Console.Clear();
+            Console.Write("Ingrese el saldo a debitar: ");
+            double pSaldo = Convert.ToDouble(Console.ReadLine());
+            if (pCuenta.DebitarSaldo(pSaldo))
+            {
+                Console.Clear();
+                Console.WriteLine("Se ha debitado exitosamente.");
+                Console.WriteLine($"Su saldo actual es: {pCuenta.Saldo} {pMoneda}.");
+                MetodosDePantalla.Continuar();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("No posee fondos suficientes para realizar esta operacion.");
+                MetodosDePantalla.Continuar();
+            }
+        }
+    }
+}
---
+++ Ejercicio02/Pantallas/PantallaTransferencia.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio02.Clases;
+using Ejercicio02.Constantes;
+using Ejercicio02.Metodos;
+
+namespace Ejercicio02.Pantallas
+{
+    public static class PantallaTransferencia
+    {
+        public static void MostrarPantallaPesosADolares(Cuenta pCuentaPesos, Cuenta pCuentaDolares)
+        {
+            double pValorDolar = ValorDolar.ValorActual;
+            double pMontoPesos;
+            double pMontoDolares;
+
+            Console.Clear();
+            Console.Write("Ingrese el monto en ARS a transferir: ");
+            pMontoPesos = Convert.ToDouble(Console.ReadLine());
+            pMontoDolares = pMontoPesos / pValorDolar;
+            pMontoDolares = Math.Round(pMontoDolares, 2);
+            
+            Console.Clear();
+            Console.WriteLine($"Usted está por transferir {pMontoPesos} ARS ({pMontoDolares} USD)");
+            Console.WriteLine();
+            Console.WriteLine("1) Confirmar operación");
+            Console.WriteLine("2) Cancelar");
+            Console.WriteLine();
+            Console.Write("Opción: ");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    if (pCuentaPesos.DebitarSaldo(pMontoPesos))
+                    {
+                        pCuentaDolares.AcreditarSaldo(pMontoDolares);
+
+                        Console.Clear();
+                        Console.WriteLine("Se ha transferido exitosamente.");
+                        Console.WriteLine($"Su saldo de cuenta en pesos es: {pCuentaPesos.Saldo} ARS.");
+                        Console.WriteLine($"Su saldo de cuenta en dolares es: {pCuentaDolares.Saldo} USD.");
+                        MetodosDePantalla.Continuar();
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("No posee fondos suficientes para realizar esta operacion.");
+                        MetodosDePantalla.Continuar();
+                    }
+                    break;
+                case "2":
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Ingrese una opción válida.");
+                    MetodosDePantalla.Continuar();
+                    break;
+            }
+        }
+
+        public static void MostrarPantallaDolaresAPesos(Cuenta pCuentaPesos, Cuenta pCuentaDolares)
+        {
+            double pValorDolar = ValorDolar.ValorActual;
+            double pMontoPesos;
+            double pMontoDolares;
+
+            Console.Clear();
+            Console.Write("Ingrese el monto en USD a transferir: ");
+            pMontoDolares = Convert.ToDouble(Console.ReadLine());
+            pMontoPesos = pMontoDolares * pValorDolar;
+
+            Console.Clear();
+            Console.WriteLine($"Usted está por transferir {pMontoDolares} USD ({pMontoPesos} ARS)");
+            Console.WriteLine();
+            Console.WriteLine("1) Confirmar operación");
+            Console.WriteLine("2) Cancelar");
+            Console.WriteLine();
+            Console.Write("Opción: ");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    if (pCuentaDolares.DebitarSaldo(pMontoDolares))
+                    {
+                        pCuentaPesos.AcreditarSaldo(pMontoPesos);
+
+                        Console.Clear();
+                        Console.WriteLine("Se ha transferido exitosamente.");
+                        Console.WriteLine($"Su saldo de cuenta en pesos es: {pCuentaPesos.Saldo} ARS.");
+                        Console.WriteLine($"Su saldo de cuenta en dolares es: {pCuentaDolares.Saldo} USD.");
+                        MetodosDePantalla.Continuar();
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("No posee fondos suficientes para realizar esta operacion.");
+                        MetodosDePantalla.Continuar();
+                    }
+                    break;
+                case "2":
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Ingrese una opción válida.");
+                    MetodosDePantalla.Continuar();
+                    break;
+            }
+        }
+    }
+}
