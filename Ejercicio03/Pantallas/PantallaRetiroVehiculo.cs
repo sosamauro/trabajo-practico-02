c25da1991b3c8d6c6c3ce9a5406c7610d7f37b97
Mauro Sosa <mauro4sosa@gmail.com>
2022-10-08 12:41:02 -0300

    Pantalla "Retiro Vehiculo"

---
+++ Ejercicio03/Pantallas/PantallaRetiroVehiculo.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio03.Clases;
+using Ejercicio03.Controladores;
+using Ejercicio03.Metodos;
+
+namespace Ejercicio03.Pantallas
+{
+    public static class PantallaRetiroVehiculo
+    {
+        public static void MostrarPantalla()
+        {
+            
+        }
+    }
+}
