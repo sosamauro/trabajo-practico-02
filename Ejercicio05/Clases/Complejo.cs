1e10444b9ce805681545e706028a2843cb4b1067
Mauro Sosa <mauro4sosa@gmail.com>
2022-10-21 19:03:29 -0300

    Ejercicio 05 terminado.

--- Ejercicio05/Clases/Complejo.cs
+++ Ejercicio05/Clases/Complejo.cs
@@ -28,79 +28,100 @@ namespace Ejercicio05.Clases
         
         public double ArgumentoEnRadianes
         {
-            get { return Math.Atan2(this.getImaginario, this.getReal); }
+            get { return Math.Atan2(this.Imaginario, this.Real); }
         }
         
         public double ArgumentoEnGrados
         {
-            get { return (this.ArgumentoEnRadianes*(180/Math.PI)); }
+            get { return (this.ArgumentoEnRadianes * (180 / Math.PI)); }
         }
         public double Conjugado
         {
-            get { return ((-1)*(this.getImaginario)); }
+            get { return ( (-1) * (this.Imaginario) ); }
         }
+
         public double Magnitud
         {
-            get { return Math.sqrt((Math.Pow(this.getReal,2))+(Math.Pow(this.getImaginario,2))) }
+            get { return Math.Sqrt((Math.Pow(this.Real, 2)) + (Math.Pow(this.Imaginario, 2))); }
         }
-        public boolean EsReal(){
-            if(this.getImaginario = 0){
+
+        public Boolean EsReal ()
+        {
+            if (this.Imaginario == 0) 
+            {
                 return true;
-            }else{
+            }
+            else
+            {
                 return false;
             }
         }
-        public boolean EsImaginario(){
-            if(not this.EsReal()){
+        public Boolean EsImaginario ()
+        {
+            if (this.EsReal() != false)
+            {
                 return true;
-            }else{
+            }
+            else
+            {
                 return false;
             }
         }
-        public boolean EsIgual(pOtroComplejo: Complejo){
-            if ((this.getReal = pOtroComplejo.getReal) & (this.getImaginario = pOtroComplejo.getImaginario)){
+        public Boolean EsIgual (Complejo pOtroComplejo)
+        {
+            if (this.Real == pOtroComplejo.Real & this.Imaginario == pOtroComplejo.Imaginario)
+            {
                 return true;
-            }else{
+            }
+            else
+            {
                 return false;
             }
         }
-        public boolean EsIgual(pReal: double, pImaginario: double){
-            if ((this.getReal = pReal) & (this.getImaginario = pImaginario)){
+        public Boolean EsIgual (double pReal, double pImaginario)
+        {
+            if (this.Real == pReal & this.Imaginario == pImaginario)
+            {
                 return true;
-            }else{
+            }
+            else
+            {
                 return false;
             }
         }
-        public Complejo Sumar(pOtroComplejo: Complejo){
-            var
-                Complejo sumaComplejo = new Complejo;
-            sumaComplejo.Complejo(pOtroComplejo.getReal + this.getReal,pOtroComplejo.getImaginario + this.getImaginario);
+        public Complejo Sumar (Complejo pOtroComplejo)
+        {
+            Complejo sumaComplejo = new Complejo(pOtroComplejo.Real + this.Real, pOtroComplejo.Imaginario + this.Imaginario);
             return sumaComplejo;
         }
-        public Complejo Restar(pOtroComplejo: Complejo){
-            var
-                Complejo restaComplejo = new Complejo;
-            restaComplejo.Complejo(pOtroComplejo.getReal - this.getReal,pOtroComplejo.getImaginario - this.getImaginario);
+
+        public Complejo Restar(Complejo pOtroComplejo)
+        {
+            Complejo restaComplejo = new Complejo(pOtroComplejo.Real - this.Real, pOtroComplejo.Imaginario - this.Imaginario);
             return restaComplejo;
         }
-        public Complejo Multiplicar(pOtroComplejo: Complejo){
-            var
-                Complejo multComplejo = new Complejo;
-                //multComplejo.Complejo(real,img);
-            multComplejo.Complejo((this.getReal*pOtroComplejo.getReal)+(-1)*(this.getImaginario*pOtroComplejo.getImaginario),(this.getReal*pOtroComplejo.getImaginario)+(this.getImaginario*pOtroComplejo.getReal));
+
+        public Complejo Multiplicar(Complejo pOtroComplejo) //(a + bi) * (c + di) = a*c + a*di + bi*c + bi*di = ac - bd + adi + bic
+        {
+            Complejo multComplejo = new Complejo((this.Real * pOtroComplejo.Real) + ((-1) * (this.Imaginario * pOtroComplejo.Imaginario)), 
+                                                 (this.Real * pOtroComplejo.Imaginario) + (this.Imaginario * pOtroComplejo.Real));
+            //multComplejo.Complejo(real,img);
             return multComplejo;
         }
-        public Complejo DividirPor(pOtroComplejo: Complejo){
-            var
-                Complejo divComplejo = new Complejo;
-                double ac, bd, ad, cb, cd;
 
-            ac = this.getReal*pOtroComplejo.getReal;
-            bd = this.getImaginario*pOtroComplejo.getImaginario;
-            ad = this.getReal*pOtroComplejo.getImaginario;
-            cb = pOtroComplejo.getReal*this.getImaginario;
-            cd = Math.Pow(pOtroComplejo.getReal,2)+Math.Pow(getImaginario.getReal,2);
-            divComplejo.Complejo((ac+bd)/cd,(cb-ad)/cd);
+        public Complejo DividirPor (Complejo pOtroComplejo)
+        {
+            
+            double ac, bd, ad, cb, cd;
+
+            ac = this.Real * pOtroComplejo.Real;
+            bd = this.Imaginario * pOtroComplejo.Imaginario;
+            ad = this.Real * pOtroComplejo.Imaginario;
+            cb = pOtroComplejo.Real * this.Imaginario;
+            cd = Math.Pow(pOtroComplejo.Real, 2) + Math.Pow(pOtroComplejo.Imaginario, 2);
+
+            Complejo divComplejo = new Complejo((ac + bd) / cd, (cb - ad) / cd);
+
             return divComplejo;
         }
     }
